751e618d626792613cdda23d82d968359334b7ad
mjh1583 <38252059+mjh1583@users.noreply.github.com>
2020-10-15 11:23:49 +0900

    Add files via upload

---
+++ workspace/Chap01(환경설정)/src/kr/co/kihd/hello/Test.java
@@ -0,0 +1,10 @@
+package kr.co.kihd.hello;
+
+public class Test {
+	
+	//Ctrl + F11 : 컴파일 후 실행시키는 단축키
+	public static void main(String[] args) {
+		System.out.println("Welcome!!");
+		System.out.println("자바 세계로 오신 것을 환영합니다");	
+	}
+}
