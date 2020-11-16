923203c2bed4220d012f6ca2801898ab5dccf4a2
mjh1583 <38252059+mjh1583@users.noreply.github.com>
2020-11-16 11:20:42 +0900

    Add files via upload

--- workspace/Chap19(유용한클래스들)/src/kr/co/kihd/calendar/TimeZoneTest.java
+++ workspace/Chap19(유용한클래스들)/src/kr/co/kihd/calendar/TimeZoneTest.java
@@ -1,5 +1,6 @@
 package kr.co.kihd.calendar;
 
+import java.util.Calendar;
 import java.util.TimeZone;
 
 public class TimeZoneTest {
@@ -12,6 +13,31 @@ public class TimeZoneTest {
 			System.out.println("타임존 영역 : " + zoneId);
 		}
 		
+		TimeZone tZone = TimeZone.getTimeZone("America/New_York");
+		Calendar today = Calendar.getInstance(tZone);
+		
+		System.out.println(toString(today));
+		
+	}
+	
+	public static String toString(Calendar date) {
+		int i = date.get(Calendar.AM_PM);
+		String str = "";
+		
+		if(i == 0) {
+			str = "AM";
+		}
+		else {
+			str = "PM";
+		}
+		
+		return date.get(Calendar.YEAR) + "년 " +
+		(date.get(Calendar.MONTH) + 1) + "월 " + 
+		date.get(Calendar.DATE) + "일" + 
+		" " + str + " " + 
+		date.get(Calendar.HOUR_OF_DAY) + "시 " +
+		date.get(Calendar.MINUTE) + "분 " +
+		date.get(Calendar.SECOND) + "초";
 	}
 	
 }
