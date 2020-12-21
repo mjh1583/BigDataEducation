6a539aadc2a8267bdf530da58658b013094c8022
JeHwan MA <38252059+mjh1583@users.noreply.github.com>
2020-12-21 17:07:58 +0900

    Add files via upload

--- workspace-query/question-1~2.sql
+++ workspace-query/question-1~2.sql
@@ -7,7 +7,7 @@
 -- -------------------------------------
 -- DEPTNO    INT            X       O
 -- DNAME     VARCHAR(14)    X       
--- LOC       VARCHAR(14)    O
+-- LOC       VARCHAR(30)    O
 
 create database mydb;
 
@@ -16,12 +16,25 @@ use mydb;
 create table dept(
 	deptno int not null,
     dname varchar(14) not null,
-    loc varchar(14),
+    loc varchar(30),
     primary key(deptno)
 );
+
+# drop table dept; 테이블 삭제
 
 desc dept;
 
 select *
 	from dept;
+    
+-- 문제 2
+-- 만들어진 DEPT테이블에 아래의 데이터를 추가하라.
+-- 10, '경리부', '서울'
+-- 20, '인사부', '인천'
+-- 30, '영업부', '용인'
+-- 40, '전산부', '수원'
 
+insert into dept values(10, '경리부', '서울');
+insert into dept values(20, '인사부', '인천');
+insert into dept values(30, '영업부', '용인');
+insert into dept values(40, '전산부', '수원');
