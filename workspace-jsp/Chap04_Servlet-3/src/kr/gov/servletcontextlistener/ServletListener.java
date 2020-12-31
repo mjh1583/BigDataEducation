eedff2b8ada5b9e7a1232d8288d5ea21336da9fd
JeHwan MA <38252059+mjh1583@users.noreply.github.com>
2020-12-31 09:29:49 +0900

    Add files via upload

--- workspace-jsp/Chap04_Servlet-3/src/kr/gov/servletcontextlistener/ServletListener.java
+++ workspace-jsp/Chap04_Servlet-3/src/kr/gov/servletcontextlistener/ServletListener.java
@@ -2,17 +2,40 @@ package kr.gov.servletcontextlistener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class ServletListener implements ServletContextListener{
 	
+	//톰캣서버 시작될때 자동 호출 됨.
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		
+		System.out.println("contextInitialized() 호출");
 	}
 	
+	//톰캣서버 중지될때 자동 호출
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		
+		System.out.println("contextDestroyed() 호출");
 	}
 	
+	/*
+	 * 위 2개의 콜백 메서드는 다른 웹어플리케이션(타 프로젝트)이 실행될때와
+	 * 종료될때 같이 실행되기 때문에 위와 같은 코드를 보여주기 원하지않으면 주석처리하면 됨.
+	 */
+	
 }
+
+/*
+ * 서블릿의 실행 순서(리스너 포함)
+ * 
+ * 1.contextInitialized() 호출
+ * 2. 생성자 호출
+ * 3.initPostConstructor() 선처리
+ * 4.init() 호출
+ * 5.service() 호출 -- doGet()호출, doPost()호출
+ * 6.destroy() 호출
+ * 7.destroyPredestroy() 후처리
+ * 8.contextDestroyed() 호출
+ * 
+ */
